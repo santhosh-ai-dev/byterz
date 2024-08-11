f3bc35dc8a7d281bbc880dda77343b8a99ad05db
SANTHOSH R <121302642+santhoshAI@users.noreply.github.com>
2024-08-11 14:05:20 +0530

    SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,13 +5,40 @@ import ThemeProvider from "@/components/ThemeProvider";
 const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
-  title: "Byterz tech",
-  description: "Byterz tech",
+  title: "Byterz Tech",
+  description: "Byters is a leading web design and development agency offering custom E-Commerce solutions, expert web development, and reliable maintenance support. We build modern, user-friendly websites that drive business growth and deliver seamless digital experiences.",
+  keywords: [
+    "Byterz tech",
+    "Byterz Technology",
+    "Best Web service in Tiruvannamalai",
+    "Tiruvannamalai Startup Companies",
+    "Startup Companies in Tiruvannamalai",
+    "Web Design and Development",
+    "Custom Web Development Services",
+    "E-Commerce Solutions Provider",
+    "Website Maintenance and Support",
+    "Professional Web Design Agency",
+    "Web Development for Businesses",
+    "Affordable E-Commerce Development",
+    "Responsive Web Design Services",
+    "Full-Service Web Development",
+    "User-Friendly Website Design",
+    "Custom E-Commerce Website Development",
+    "Web Design and Maintenance Solutions",
+    "Digital Solutions for Businesses",
+    "Modern Web Development Company",
+    "Comprehensive Web Support Services"
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="keywords" content={metadata.keywords.join(", ")} />
+        <meta name="description" content={metadata.description} />
+        <link rel="canonical" href="https://byterz-tech.netlify.app/" />
+      </head>
       <body className={dmSans.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
